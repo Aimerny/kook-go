20608a65548861a75a7ab2a63fc6c9f3fa8044b9
夏至 <tanchjim@aol.com>
2024-07-05 02:38:42 +0800

    feat:✨ 更新消息发送与更新消息功能

--- app/core/action/message.go
+++ app/core/action/message.go
@@ -1,8 +1,12 @@
 package action
 
 import (
+	"encoding/json"
+
 	"github.com/aimerny/kook-go/app/common"
+	"github.com/aimerny/kook-go/app/core/helper"
 	"github.com/aimerny/kook-go/app/core/model"
+	"github.com/sirupsen/logrus"
 )
 
 func MessageList() {
@@ -13,12 +17,31 @@ func MessageDetail() {
 
 }
 
-func MessageSend(req *model.MessageCreateReq) {
-	go doPost(common.MessageCreate, req)
+func MessageSend(req *model.MessageCreateReq) *model.MessageCreateResp {
+	response, err := helper.Post(common.BaseUrl+common.V3Url+common.MessageCreate, &req)
+	if err != nil {
+		return nil
+	}
+	var result *model.KookResponse[*model.MessageCreateResp]
+	err = json.Unmarshal(response, &result)
+	if result.Code != 0 {
+		return nil
+	}
+	return result.Data
 }
 
-func MessageUpdate() {
-
+func MessageUpdate(req *model.MessageUpdateReq) error {
+	logrus.Println("开始更新消息msg_id:", req.MsgId)
+	response, err := helper.Post(common.BaseUrl+common.V3Url+common.MessageUpdate, &req)
+	if err != nil {
+		return err
+	}
+	var result *model.KookResponse[interface{}]
+	err = json.Unmarshal(response, &result)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func MessageDelete() {
